774fcc08338d75ff3b5499d6deb63f8a40326b36
Stefan Wimmer <info@stefanwimmer128.xyz>
2025-11-06 20:59:10 +0100

    feat(settings): Add description to skins and increase image size

--- settings/src/pages/design/Skin.vue
+++ settings/src/pages/design/Skin.vue
@@ -17,50 +17,65 @@
         <q-list class="q-mb-lg">
             <q-item tag="label" clickable v-ripple>
                 <q-item-section avatar>
-                    <q-img src="@/assets/design/skin/proton-dark.svg" width="100px" v-if="dark.isActive" />
-                    <q-img src="@/assets/design/skin/proton-light.svg" width="100px" v-else />
+                    <q-img src="@/assets/design/skin/proton-dark.svg" width="150px" v-if="dark.isActive" />
+                    <q-img src="@/assets/design/skin/proton-light.svg" width="150px" v-else />
                 </q-item-section>
-                <q-item-section>{{ t('pages.design.skin.proton.title') }}</q-item-section>
+                <q-item-section>
+                    <q-item-label>{{ t('pages.design.skin.proton.title') }}</q-item-label>
+                    <q-item-label caption>{{ t('pages.design.skin.proton.description') }}</q-item-label>
+                </q-item-section>
                 <q-item-section side>
                     <q-radio v-model="skin" val="" />
                 </q-item-section>
             </q-item>
             <q-item tag="label" clickable v-ripple>
                 <q-item-section avatar>
-                    <q-img src="@/assets/design/skin/fluerial-dark.svg" width="100px" v-if="dark.isActive" />
-                    <q-img src="@/assets/design/skin/fluerial-light.svg" width="100px" v-else />
+                    <q-img src="@/assets/design/skin/fluerial-dark.svg" width="150px" v-if="dark.isActive" />
+                    <q-img src="@/assets/design/skin/fluerial-light.svg" width="150px" v-else />
                 </q-item-section>
-                <q-item-section>{{ t('pages.design.skin.fluerial.title') }}</q-item-section>
+                <q-item-section>
+                    <q-item-label>{{ t('pages.design.skin.fluerial.title') }}</q-item-label>
+                    <q-item-label caption>{{ t('pages.design.skin.fluerial.description') }}</q-item-label>
+                </q-item-section>
                 <q-item-section side>
                     <q-radio v-model="skin" val="fluerial" />
                 </q-item-section>
             </q-item>
             <q-item tag="label" clickable v-ripple>
                 <q-item-section avatar>
-                    <q-img src="@/assets/design/skin/lepton-dark.svg" width="100px" v-if="dark.isActive" />
-                    <q-img src="@/assets/design/skin/lepton-light.svg" width="100px" v-else />
+                    <q-img src="@/assets/design/skin/lepton-dark.svg" width="150px" v-if="dark.isActive" />
+                    <q-img src="@/assets/design/skin/lepton-light.svg" width="150px" v-else />
                 </q-item-section>
-                <q-item-section>{{ t('pages.design.skin.lepton.title') }}</q-item-section>
+                <q-item-section>
+                    <q-item-label>{{ t('pages.design.skin.lepton.title') }}</q-item-label>
+                    <q-item-label caption>{{ t('pages.design.skin.lepton.description') }}</q-item-label>
+                </q-item-section>
                 <q-item-section side>
                     <q-radio v-model="skin" val="lepton" />
                 </q-item-section>
             </q-item>
             <q-item tag="label" clickable v-ripple>
                 <q-item-section avatar>
-                    <q-img src="@/assets/design/skin/lepton-photon-dark.svg" width="100px" v-if="dark.isActive" />
-                    <q-img src="@/assets/design/skin/lepton-photon-light.svg" width="100px" v-else />
+                    <q-img src="@/assets/design/skin/lepton-photon-dark.svg" width="150px" v-if="dark.isActive" />
+                    <q-img src="@/assets/design/skin/lepton-photon-light.svg" width="150px" v-else />
                 </q-item-section>
-                <q-item-section>{{ t('pages.design.skin.lepton-photon.title') }}</q-item-section>
+                <q-item-section>
+                    <q-item-label>{{ t('pages.design.skin.lepton-photon.title') }}</q-item-label>
+                    <q-item-label caption>{{ t('pages.design.skin.lepton-photon.description') }}</q-item-label>
+                </q-item-section>
                 <q-item-section side>
                     <q-radio v-model="skin" val="lepton-photon" />
                 </q-item-section>
             </q-item>
             <q-item tag="label" clickable v-ripple>
                 <q-item-section avatar>
-                    <q-img src="@/assets/design/skin/lepton-proton-dark.svg" width="100px" v-if="dark.isActive" />
-                    <q-img src="@/assets/design/skin/lepton-proton-light.svg" width="100px" v-else />
+                    <q-img src="@/assets/design/skin/lepton-proton-dark.svg" width="150px" v-if="dark.isActive" />
+                    <q-img src="@/assets/design/skin/lepton-proton-light.svg" width="150px" v-else />
                 </q-item-section>
-                <q-item-section>{{ t('pages.design.skin.lepton-proton.title') }}</q-item-section>
+                <q-item-section>
+                    <q-item-label>{{ t('pages.design.skin.lepton-proton.title') }}</q-item-label>
+                    <q-item-label caption>{{ t('pages.design.skin.lepton-proton.description') }}</q-item-label>
+                </q-item-section>
                 <q-item-section side>
                     <q-radio v-model="skin" val="lepton-proton" />
                 </q-item-section>
