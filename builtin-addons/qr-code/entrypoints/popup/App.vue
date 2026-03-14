5f05ba5d7377018df5a2eaa7fbf8b0afc1a6fcef
Stefan Wimmer <info@stefanwimmer128.xyz>
2026-03-14 19:58:32 +0100

    feat: Add QR code generation page action popup

---
+++ builtin-addons/qr-code/entrypoints/popup/App.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+    import { useHead } from '@unhead/vue';
+    import QRCodeStyling from 'qr-code-styling';
+
+    const { t } = useI18n();
+
+    useHead({
+        title: t('title'),
+    });
+
+    const container = ref<HTMLCanvasElement>();
+
+    onMounted(async () => {
+        const [tab, image] = await Promise.all([
+            browser.tabs.query({ active: true, currentWindow: true }),
+            browser.browser.getLogo(),
+        ]);
+        if (container.value && tab[0]) {
+            const color = getComputedStyle(container.value)?.getPropertyValue('--q-primary');
+            console.log(color);
+            const qrqCode = new QRCodeStyling({
+                width: 300,
+                height: 300,
+                type: 'canvas',
+                data: tab[0].url,
+                image,
+                backgroundOptions: {
+                    color: 'transparent',
+                },
+                dotsOptions: {
+                    color: color,
+                    type: 'rounded',
+                },
+            });
+            qrqCode.append(container.value);
+        }
+    });
+</script>
+
+<template>
+    <suspense>
+        <q-layout view="hHh LpR fFf">
+            <q-page-container>
+                <q-page>
+                    <div ref="container" />
+                </q-page>
+            </q-page-container>
+        </q-layout>
+    </suspense>
+</template>
---
+++ builtin-addons/qr-code/wxt.config.ts
@@ -0,0 +1,45 @@
+import { URL, fileURLToPath } from 'node:url';
+
+import defineConfig from '@firedragon/build/wxt-config';
+import vueI18n from '@intlify/unplugin-vue-i18n/vite';
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin';
+import devtools from 'vite-plugin-vue-devtools';
+
+import { getEdition } from '../../config';
+
+export default defineConfig({
+    modules: ['@wxt-dev/module-vue'],
+    firedragon: {
+        id: 'firedragon-qr-code',
+    },
+    imports: {
+        presets: ['@vueuse/core', 'quasar', 'vue-i18n'],
+    },
+    vue: {
+        vite: {
+            template: {
+                transformAssetUrls,
+            },
+        },
+    },
+    manifest: {
+        name: 'FireDragon QR Code',
+        permissions: ['activeTab'],
+        page_action: {
+            show_matches: ['<all_urls>'],
+        },
+    },
+    vite: () => ({
+        plugins: [
+            vueI18n({
+                include: [fileURLToPath(new URL('./assets/locales/*', import.meta.url))],
+            }),
+            quasar({
+                sassVariables: getEdition()?.quasar,
+            }),
+            devtools({
+                appendTo: /\/entrypoints\/(index|popup|sidepanel)\/main.ts$/,
+            }),
+        ],
+    }),
+});
