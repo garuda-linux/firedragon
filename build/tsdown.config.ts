c71f1294e7da1bc33b869676bed2c91dd0258137
Stefan Wimmer <info@stefanwimmer128.xyz>
2026-03-14 13:05:27 +0100

    refactor: Let tsdown manage exports in package.json for build & shared

--- build/tsdown.config.ts
+++ build/tsdown.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'tsdown';
 
 export default defineConfig({
-    entry: 'src/**/*.ts',
+    entry: ['src/vite.ts', 'src/wxt-client.ts', 'src/wxt-config.ts', 'src/wxt-module.ts'],
     dts: true,
+    exports: true,
 });
--- shared/tsdown.config.ts
+++ shared/tsdown.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from 'tsdown';
 
 export default defineConfig({
-    entry: 'src/**/*.ts',
+    entry: [
+        'src/experiment-api/prefs.ts',
+        'src/types/keyboard-shortcuts.ts',
+        'src/vue/createStorage.ts',
+        'src/vue/usePref.ts',
+    ],
     dts: true,
+    exports: true,
 });
