6d2fa79b50601d0002dee7beea90b4cc4484eb25
Stefan Wimmer <info@stefanwimmer128.xyz>
2026-08-19 21:08:48 +0200

    fix: Use browser locale for builtin addons

--- builtin-addons/workspaces/utils/i18n.ts
+++ builtin-addons/workspaces/utils/i18n.ts
@@ -2,6 +2,7 @@ import messages from '@intlify/unplugin-vue-i18n/messages';
 import { createI18n } from 'vue-i18n';
 
 const i18n = createI18n({
+    locale: navigator.language,
     fallbackLocale: 'en-US',
     messages,
 });
