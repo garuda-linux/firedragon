bdfbe1274ccdb6145692523192b2cf10bfca604e
Stefan Wimmer <info@stefanwimmer128.xyz>
2026-08-19 18:26:45 +0200

    refactor: Use patches to manage visibility of password manager preferences

--- content/src/preferences/hidePasswdmgr.ts
+++
@@ -1,6 +0,0 @@
-document!.addEventListener('paneshown', function listener(e: CustomEvent) {
-    if (e.detail.category === 'panePrivacy' && Services.prefs.getBoolPref('firedragon.hidePasswdmgr')) {
-        document!.removeEventListener('paneshown', listener);
-        document!.querySelector('#passwordsGroup')!.remove();
-    }
-});
